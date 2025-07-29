006f067faae9065c3a534838e08ebfe6d11fc41b
sara <sara.atef001@gmail.com>
2025-07-29 11:22:09 +0300

    Day04

---
+++ JS/Day04/demo.js
@@ -0,0 +1,67 @@
+/**
+ * == change style
+ * ----------------------
+ *  - style
+ *  -- className
+ *  -- classList
+ *      -- add
+ *      -- remove
+ *      -- toggle
+ *
+ * -- setAttribute
+ *  -- setAttributes
+ *  -- removeAttributes
+ *  -- toggleAttributes
+ *  -- hasAttribute
+ */
+// var p = document.getElementsByClassName("info")[0];
+// function changeStyle() {
+// 	p.style.backgroundColor = "red";
+// 	p.style.padding = "10px";
+// 	p.style.letterSpacing = "2px";
+// }
+// v2
+// function changeStyle() {
+// 	p.className += " jsClass";
+// 	// console.log(p.className);
+// }
+// v3
+// function changeStyle() {
+// 	p.classList.toggle("jsClass");
+// 	// p.classList.add("jsClass");
+// 	// p.classList.remove("testData");
+// 	// console.log(p.classList);
+// 	//p.className += " jsClass";
+// 	// console.log(p.className);
+// }
+
+// var link1 = document.getElementById("link1");
+
+// function execute() {
+// 	if (link1.hasAttribute("class")) {
+// 		link1.removeAttribute("class");
+// 	} else {
+// 		link1.setAttribute("class", "jsClass");
+// 	}
+// 	// link1.setAttribute("href", "#twitter");
+// 	// link1.setAttribute("class", "testData");
+// 	// console.log(link1.getAttribute("class"));
+// }
+
+// var img = document.images[0];
+
+// function execute() {
+// 	img.setAttribute("src", "5.jpg");
+// 	// console.log(img.getAttribute("src"));
+// }
+
+// var trackName = "dotnet";
+
+// debugger;
+// console.log("Hello at iti");
+
+// for (var i = 0; i < 4; i++) {
+// 	console.log(i);
+// }
+
+// console.log("End Of execution");
---
+++ JS/Day04/main.js
@@ -0,0 +1,50 @@
+// var div = document.getElementById("parent");
+
+// console.log(div.children);
+// console.log(div.childNodes);
+
+// console.log(div.firstChild);
+// console.log(div.firstElementChild);
+// console.log(div.lastChild);
+// console.log(div.lastElementChild);
+// console.log(document.images);
+// console.log(document.images[0]);
+// console.log(document.images[1]);
+// console.log(document.images[2]);
+// console.log(document.body);
+
+// var div = document.getElementById("parent");
+
+// var element = document.querySelector(".info");
+
+// console.log(element);
+// for (var i = 0; i < elements.length; i++) {
+// 	console.log(elements[i]);
+// }
+// var inputs = document.getElementsByName("gender");
+
+// var span = document.getElementById("output");
+
+// //v2
+// function showAnswer() {
+// 	for (var i = 0; i < inputs.length; i++) {
+// 		if (inputs[i].checked) {
+// 			span.innerHTML = inputs[i].value;
+// 		}
+// 	}
+// }
+//v1
+// function showAnswer() {
+// 	span.innerHTML = input1.value;
+// 	// input1.value = "Dotnet track";
+// 	// get
+// 	// console.log(input1.value); //
+// }
+// var p = document.getElementsByClassName("info")[0];
+// function changeText() {
+// 	// get
+// 	// console.log(p.innerText);
+// 	// console.log(p.innerHTML);
+// 	//p.innerText = "<h1>Welcome at ITI</h1>";
+// 	// p.innerHTML = "<h1>Welcome at ITI</h1>";
+// }
